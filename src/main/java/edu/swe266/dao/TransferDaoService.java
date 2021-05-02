4886e6d128f70f40d78d6910322aea7209b1d9e4
duke326 <448711277@qq.com>
2021-05-01 23:27:10 -0700

    create Dao sevices

---
+++ src/main/java/edu/swe266/dao/TransferDaoService.java
@@ -0,0 +1,36 @@
+package edu.swe266.dao;
+
+
+public interface TransferDaoService {
+    /**
+     *
+     * @param account user's account
+     * @param amount amount want to withdraw from Bank
+     */
+    void withdrawFromBank(int account, int amount);
+
+    /**
+     *
+     * @param account user's account
+     * @param amount amount want to deposit to Bank
+     */
+    void depositToBank(int account, int amount);
+
+    /**
+     *
+     * @param balance Bank's balance
+     * @param amount  operation's amount
+     * @return whether balance has sufficient money
+     */
+
+    boolean checkBankBalance(int balance, int amount);
+
+    /**
+     *
+     * @param balance user's balance
+     * @param amount operation's amount
+     * @return whether user has sufficient money
+     */
+    boolean checkAccountBalance(int balance, int amount);
+
+}
---
+++ src/main/java/edu/swe266/dao/UserDaoSevice.java
@@ -0,0 +1,26 @@
+package edu.swe266.dao;
+
+public interface UserDaoSevice {
+     /**
+      *
+      * @param account user's account
+      * @return balance of the account
+      */
+     int checkBalance(int account);
+
+     void createAccount(int account, int psw);
+
+     void modifyAddress(int account, String newInfo);
+
+     void modifyDOB(int account, String newInfo);
+
+     void modifyName(int account, String newInfo);
+
+     /**
+      *
+      * @param account user's account
+      * @return whether this account has already exist
+      */
+     boolean checkAccountExist(int account);
+
+}
