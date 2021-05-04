1ee8a8459a5dec55c17ba636f762a9809cab0e65
LaiWang2020 <laiw13@uci.edu>
2021-05-03 18:10:08 -0700

    README.md update

--- src/main/java/edu/swe266/service/impl/MainServiceImpl.java
+++ src/main/java/edu/swe266/service/impl/MainServiceImpl.java
@@ -1,4 +1,15 @@
 package edu.swe266.service.impl;
 
-public class MainServiceImpl {
+import edu.swe266.dao.UserMapper;
+import edu.swe266.service.IMainService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MainServiceImpl implements IMainService {
+
+    @Autowired
+    UserMapper mapper;
+
+    public void foo(){
+
+    }
 }
