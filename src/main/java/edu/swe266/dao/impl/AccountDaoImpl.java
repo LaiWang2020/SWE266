e746879d3998ce07e340b4edbb864846531efae4
duke326 <448711277@qq.com>
2021-05-07 11:07:41 -0700

    update dao operation

--- src/main/java/edu/swe266/dao/impl/AccountDaoImpl.java
+++ src/main/java/edu/swe266/dao/impl/AccountDaoImpl.java
@@ -15,28 +15,23 @@ public class AccountDaoImpl implements AccountDao {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    private static final double DEFAULT_DEPOSIT=100;
 
     @Override
-    public double checkDeposit(String user) {
-        /*Good code here
+    public double checkDeposit(String username) {
+
         Account account=jdbcTemplate.queryForObject("Select deposit from account where username=?", new BeanPropertyRowMapper<Account>(Account.class), "duke");
         System.out.println("invoke success");
-        return account.getDeposit();*/
-
-        /*Bad Code start*/
-        Account account=jdbcTemplate.queryForObject("Select deposit from account where username='"+user+"'", new BeanPropertyRowMapper<Account>(Account.class));
-        System.out.println("invoke success");
         return account.getDeposit();
-        /*Bad Code end*/
     }
 
 
 
     @Override
-    public void deposit(String user, double money) {
-         Account account=jdbcTemplate.queryForObject("Select deposit from account where username=?", new BeanPropertyRowMapper<Account>(Account.class), user);
+    public void deposit(String username, double money) {
+         Account account=jdbcTemplate.queryForObject("Select deposit from account where username=?", new BeanPropertyRowMapper<Account>(Account.class), username);
          double deposit= account.getDeposit();
-        jdbcTemplate.update("update account set deposit=? where username=?", money+deposit, user);
+        jdbcTemplate.update("update account set deposit=? where username=?", money+deposit, username);
         //bad code
         System.out.println("Deposit "+money+"successful");
     }
@@ -44,29 +39,49 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     //should add check part
-    public void withDraw(String user, double money) {
-        Account account=jdbcTemplate.queryForObject("Select deposit from account where username=?", new BeanPropertyRowMapper<Account>(Account.class), user);
-        double deposit= account.getDeposit();
-        jdbcTemplate.update("update account set deposit=? where username=?", deposit-money, user);
+    public void withDraw(String username, double money) {
+        double deposit= checkDeposit(username);
+        //bad code
+        if(deposit-money<0){
+            System.out.println("fail! ");
+            return;
+        }
+        jdbcTemplate.update("update account set deposit=? where username=?", deposit-money, username);
         //bad code
         System.out.println("Withdraw "+money+"successful");
     }
 
     @Override
-    public void createAccount(String user, String psw) {
-        if(!userExist(user)){
-
+    public void createAccount( String username,String psw) {
+        if(!userExist(username)){
+            jdbcTemplate.update("INSERT INTO account (psw,username,deposit) values(?,?,?)", psw,username,DEFAULT_DEPOSIT);
         }
     }
 
     @Override
-    public boolean logIn(String user, String psw) {
-        return false;
+    public boolean logIn(String username, String psw) {
+//        String sqlQuery = "select username, password, password_hint, created_at, last_login, real_name, blab_name from users where username='"
+//                + username + "' and password='" + md5(password) + "';";
+        //bad code SQL injection
+        List<Account> accounts = jdbcTemplate.query("Select * from account where username='" + username + "' and psw='" + psw + "'", new BeanPropertyRowMapper<Account>(Account.class));
+        if(accounts.size()==0){
+            System.out.println("User not found, please check your psw or username");
+            return false;
+        }
+        else{
+            System.out.println("login success");
+            return true;
+        }
+
     }
 
     @Override
-    public boolean userExist(String user) {
-        //jdbcTemplate.query("Select"username"from ");
-        return false;
+    public boolean userExist(String username) {
+        List<Account> accounts = jdbcTemplate.query("Select username from account where username=?", new BeanPropertyRowMapper<Account>(Account.class), username);
+        //bad code
+        if(accounts.size()==0){
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/edu/swe266/service/AccountService.java
+++ src/main/java/edu/swe266/service/AccountService.java
@@ -2,10 +2,10 @@ package edu.swe266.service;
 
 public interface AccountService {
 
-    public void withdrawMoney(String user, double money);
-    public void depositMoney(String user, double money);
-    public double checkDeposit(String user);
-    public void createAccount(String user, String psw);
-    public boolean logIn(String user, String psw);
-    public boolean userExist(String user);
+    public void withdrawMoney(String username, double money);
+    public void depositMoney(String username, double money);
+    public double checkDeposit(String username);
+    public void createAccount(String username,String psw);
+    public boolean logIn(String username, String psw);
+    public boolean userExist(String username);
 }
