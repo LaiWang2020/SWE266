24c1188a4665462d9bf3e34acec57db6c80d225a
LaiWang2020 <laiw13@uci.edu>
2021-05-08 10:42:13 -0700

    finish MainController

--- src/main/java/edu/swe266/controller/MainController.java
+++ src/main/java/edu/swe266/controller/MainController.java
@@ -26,12 +26,12 @@ public class MainController {
         //这里如果不判断，直接加入到session里面其实是可以当成一个问题的，就是那个cwe trust boundary
         if(isSuccess){
             session.setAttribute(Const.CURRENT_USER,username);
+            return Const.SUCCESS;
         }
-        return Const.SUCCESS;
+        return Const.FAILURE;
     }
 
     @RequestMapping(value = "logout",method = RequestMethod.POST)
-    @ResponseBody
     public String logout(HttpSession session){
         session.removeAttribute(Const.CURRENT_USER);
         return Const.SUCCESS;
@@ -39,27 +39,36 @@ public class MainController {
 
     @RequestMapping(value = "/signUp",method = RequestMethod.POST)
     public String signUp(String username, String password, HttpSession session) {
-        //boolean isSuccess = accountService.createAccount(username,password);
+        boolean isSuccess = accountService.createAccount(username,password);
         //这里如果不判断，直接加入到session里面其实是可以当成一个问题的，就是那个cwe trust boundary
-//        if(isSuccess){
-//            session.setAttribute(Const.CURRENT_USER,username);
-//        }
-        return Const.SUCCESS;
+        if(isSuccess){
+            session.setAttribute(Const.CURRENT_USER,username);
+            return Const.SUCCESS;
+        }
+        return Const.FAILURE;
     }
 
     @RequestMapping(value = "/forgot")
     public String forgot() { return "forgot"; }
 
     @RequestMapping(value = "/deposit",method = RequestMethod.POST)
-    public String deposit(String username,double amount) {
-        accountService.depositMoney(username,amount);
-        return Const.SUCCESS;
+    public String deposit(double amount,HttpSession session) {
+        String username = (String) session.getAttribute(Const.CURRENT_USER);
+        boolean isSuccess = accountService.depositMoney(username,amount);
+        if(isSuccess){
+            return Const.SUCCESS;
+        }
+        return Const.FAILURE;
     }
 
     @RequestMapping(value = "/withdraw",method = RequestMethod.POST)
-    public String withdraw(String username,double amount) {
-        accountService.withdrawMoney(username,amount);
-        return Const.SUCCESS;
+    public String withdraw(double amount,HttpSession session) {
+        String username = (String) session.getAttribute(Const.CURRENT_USER);
+        boolean isSuccess = accountService.withdrawMoney(username,amount);
+        if(isSuccess){
+            return Const.SUCCESS;
+        }
+        return Const.FAILURE;
     }
 
 
