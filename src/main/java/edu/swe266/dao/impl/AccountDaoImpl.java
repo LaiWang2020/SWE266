74e6d6dcf3dbfbd9c78570a4844f523dffe9e233
duke326 <448711277@qq.com>
2021-06-02 10:11:39 +0800

    fix CWE 200 and SQL injection

--- src/main/java/edu/swe266/dao/impl/AccountDaoImpl.java
+++ src/main/java/edu/swe266/dao/impl/AccountDaoImpl.java
@@ -24,11 +24,11 @@ public class AccountDaoImpl implements AccountDao {
     public double checkDeposit(String username) {
         notNull(username);
         if(!userExist(username)){
-            System.out.println("User doesn't exist");
+
             return 0;
         }
         Account account=jdbcTemplate.queryForObject("Select deposit from account where username=?", new BeanPropertyRowMapper<Account>(Account.class), username);
-        System.out.println("invoke success");
+
         return account.getDeposit();
     }
     
@@ -37,18 +37,18 @@ public class AccountDaoImpl implements AccountDao {
         notNull(username);
         notNull(money);
         if(!userExist(username)){
-            System.out.println("User doesn't exist");
+
             return false;
         }
         if(money<0){
-            System.out.println("Please enter the valid money");
+
             return false;
         }
          Account account=jdbcTemplate.queryForObject("Select deposit from account where username=?", new BeanPropertyRowMapper<Account>(Account.class), username);
          double deposit= account.getDeposit();
         jdbcTemplate.update("update account set deposit=? where username=?", money+deposit, username);
         //bad code
-        System.out.println("Deposit "+money+"successful");
+
         return true;
     }
 
@@ -59,19 +59,17 @@ public class AccountDaoImpl implements AccountDao {
         notNull(username);
         notNull(money);
         if(!userExist(username)){
-            System.out.println("User doesn't exist");
+
             return false;
         }
         double deposit= checkDeposit(username);
-        System.out.println("deposit is "+deposit);
-        //bad code
+
         if(deposit-money<0||money<0){
-            System.out.println("operation fail!");
+
             return false;
         }
         jdbcTemplate.update("update account set deposit=? where username=?", deposit-money, username);
-        //bad code
-        System.out.println("Withdraw "+money+"successful");
+
         return true;
     }
 
@@ -81,7 +79,7 @@ public class AccountDaoImpl implements AccountDao {
         notNull(psw);
         if(!userExist(username)){
             jdbcTemplate.update("INSERT INTO account (psw,username,deposit) values(?,?,?)", psw,username,DEFAULT_DEPOSIT);
-            System.out.println("Create success");
+
             return true;
         }
         return false;
@@ -92,15 +90,14 @@ public class AccountDaoImpl implements AccountDao {
         notNull(username);
         notNull(psw);
         //good code
-        //List<Account> accounts=jdbcTemplate.query("select * from account where username=? and psw=?",new BeanPropertyRowMapper<Account>(Account.class), username,psw);
+        List<Account> accounts=jdbcTemplate.query("select * from account where username=? and psw=?",new BeanPropertyRowMapper<Account>(Account.class), username,psw);
         //bad code SQL injection
-        List<Account> accounts = jdbcTemplate.query("Select * from account where username='" + username + "' and psw='" + psw + "'", new BeanPropertyRowMapper<Account>(Account.class));
+        //List<Account> accounts = jdbcTemplate.query("Select * from account where username='" + username + "' and psw='" + psw + "'", new BeanPropertyRowMapper<Account>(Account.class));
         if(accounts.size()==0){
-            System.out.println("User not found, please check your psw or username");
             return false;
         }
         else{
-            System.out.println("login success");
+
             return true;
         }
 
@@ -114,7 +111,7 @@ public class AccountDaoImpl implements AccountDao {
         if(accounts.size()==0){
             return false;
         }
-        System.out.println("User exist! ");
+
         return true;
     }
 }
